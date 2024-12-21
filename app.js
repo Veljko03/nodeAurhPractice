8332fd660dbb7a544499ffbefa642e76f9bfc29d
Veljko03 <veljkodovragovic@gmail.com>
2024-12-21 13:45:00 +0100

    inicialized node app with passport and connected it to db, made siplme sing form and inserted them into db

---
+++ app.js
@@ -0,0 +1,41 @@
+/////// app.js
+require("dotenv").config();
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const pool = new Pool({
+  host: process.env.HOST, // or wherever the db is hosted
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT, // The default port
+});
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+      req.body.username,
+      req.body.password,
+    ]);
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
